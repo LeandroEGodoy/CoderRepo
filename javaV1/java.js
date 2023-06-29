8370d846c6b4052af2501cbfc60d0f3c873625d3
LeandroEGodoy <cdilean@gmail.com>
2023-06-29 19:46:13 -0300

    Carrito de Compras

--- javaV1/java.js
+++ javaV1/java.js
@@ -1,44 +1,46 @@
-let intentos = 3;
-let intentosFallidos = 0;
-let ingresoExitoso = false;
-let caracteresPermitidos = /^[A-Za-z]+$/;
-
-for (let i = 0; i < intentos; i++) {
-  let nombre = prompt("Ingresa tu nombre: ");
-
-  if (!nombre || !nombre.match(caracteresPermitidos)) {
-    console.log("No respondiste lo pedido o nombre inválido.");
-    alert("No respondiste lo pedido o nombre inválido.");
-    intentosFallidos++;
-    continue;
-  }
-
-  let edad = parseInt(prompt("Ingresa tu edad: "));
-
-  if (isNaN(edad) || edad <= 0) {
-    console.log("No respondiste lo pedido o edad inválida.");
-    alert("No respondiste lo pedido o edad inválida.");
-    intentosFallidos++;
-    continue;
-  }
-
-  if (edad >= 18) {
-    console.log("Bienvenido/a, " + nombre + "! Podes entrar a la página.");
-    alert("Bienvenido/a, " + nombre + "! Podes entrar a la página.");
-    ingresoExitoso = true;
-    break;
-  } else {
-    console.log(nombre + ", no podes ingresar a la página.");
-    alert(nombre + ", no podes ingresar a la página.");
-    break;
+class Producto {
+  constructor(id, nombre, precio, description){
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.description = description;
   }
 }
 
-if (!ingresoExitoso && intentosFallidos === intentos) {
-  console.log("Llegaste al máximo de intentos. No podes ingresar a la página.");
-  alert("Llegaste al máximo de intentos. No podes ingresar a la página.");
-}
+const productos = [
+  new Producto(1, 'Croma Ragnarok', 30 , 'Gama de colores para Skins'),
+  new Producto(2, 'Remera Ragnarok', 250 , 'Indumentaria de Ragnarok'),
+  new Producto(3, 'Class Knigth', 730 , 'Figura de ragnarok'),
+  new Producto(4, 'Class Rogue', 180 , 'Figura de ragnarok'),
+  new Producto(5, 'Class High Priest', 135 , 'Figura de ragnarok'),
+  new Producto(6, 'Class Lord Knigth', 150 , 'Figura de ragnarok'),
+  new Producto(7, 'Class Thief', 250 , 'Figura de ragnarok'),
+  new Producto(8, 'Class MVP', 150 , 'Figura de ragnarok'),
+  new Producto(9, 'Class Stalker', 200 , 'Figura stalker'),
+  new Producto(10, 'Pinn Ragnarok', 20 , 'Ping para mochila, etc.'),
+];
+const productContainer = document.querySelector('#product-container');
 
+productos.forEach(producto => {
+  const productDiv = document.createElement('div');
+  productDiv.classList.add(`producto`);
+  productDiv.style.backgroundColor = '#FF0000';
+  //innerHTML
+  productDiv.innerHTML = `
+    <div class='card'>
+      <h3>${producto.id}</h3>
+      <h1>${producto.nombre}</h1>
+      <p>${producto.precio}</p>
+      <p>${producto.description}</p>
+      <button class="agregar-carrito"> Agregar al carro</button>
+    </div>
+  `;
 
-if (ingresoExitoso) {
-   window.location.href = "https://chat.openai.com/";}
+  productContainer.appendChild(productDiv);
+
+  const addButton = productDiv.querySelector('.agregar-carrito');
+  addButton.addEventListener('click', () => {
+  console.log('Se agrego al carrito', producto);
+  });
+
+})
